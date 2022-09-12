231b4ddcb48210a5208c3db98f5e3251a4a5b66d
tahany777 <tahanymohammed777@hotmail.com>
2022-09-12 16:35:24 +0300

    add dynamic images

--- cars/src/components/ImageList.js
+++ cars/src/components/ImageList.js
@@ -1,5 +1,6 @@
+import './ImageList.css';
 import React from "react";
-
+import ImageCard from './ImageCard';
 //way One
 // const ImageList = (props) => {
 //     const images = props.images.map((img) => {
@@ -9,11 +10,17 @@ import React from "react";
 // };
 
 //way Two
-const ImageList = (props) => {
-    const images = props.images.map(({ description, id, urls }) => {
-        return <img key={id} src={urls.regular} alt={description}/>
-    });
-    return <div>{images}</div>
-};
+// const ImageList = (props) => {
+//     const images = props.images.map(({ description, id, urls }) => {
+//         return <img key={id} src={urls.regular} alt={description}/>
+//     });
+//     return <div className='image-list'>{images}</div>
+// };
 
+const ImageList = (props) => {
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image} />
+    });
+    return <div className='image-list'>{images}</div>
+};
 export default ImageList;
