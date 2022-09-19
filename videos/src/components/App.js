4af13300d33e4c02d70bf47ab1b28b9a7c07c1a8
tahany777 <tahanymohammed777@hotmail.com>
2022-09-19 07:38:40 +0300

    feat: added iframe for youtube video

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
@@ -14,12 +15,13 @@ class App extends React.Component {
         this.setState({ videos: response.data.items});
     }
     onVideoSelect = (video) => {
-        console.log('From the App !', video);
+        this.setState({ selectedVideo: video });
     }
     render(){
         return (
         <div className="ui container">
             <SearchBar onFormSubmit={this.onTextSubmit} />
+            <VideoDetail video={this.state.selectedVideo} />
             <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
         </div>
         );
