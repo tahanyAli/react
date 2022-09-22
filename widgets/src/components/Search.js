e82af7e88b7002891abbc647c74e7d9d8d9ae00c
tahany777 <tahanymohammed777@hotmail.com>
2022-09-22 13:15:53 +0300

    feat: add search component with functionality

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);//Array of objects
+  useEffect(() => {
+    const searchW = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);//rerender component
+    }
+    if(term) {
+      searchW();
+    }
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a 
+          className="ui button"
+          href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+        </div>
+      </div>
+    )
+  })
   return (
     <div>
       <div className="ui form">
@@ -14,6 +52,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
