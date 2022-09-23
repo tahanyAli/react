cca0a179fe39ec14fc3670fff9f47b911e03a110
tahany777 <tahanymohammed777@hotmail.com>
2022-09-23 17:46:53 +0300

    added dropdown menu

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 // import Accordion from "./components/Accordion";
-import Search from './components/Search';
+// import Search from './components/Search';
+import Dropdown from "./components/Dropdown";
 
 // const items = [
 //     {
@@ -17,11 +18,36 @@ import Search from './components/Search';
 //     }
 
 // ];
+const options = [
+    {
+        label: 'The color Red',
+        value: 'red'
+    },
+    {
+        label: 'The color Green',
+        value: 'green'
+    },
+    {
+        label: 'A shade of Blue',
+        value: 'blue'
+    }
+];
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
     return (
         <div>
-            <h1><Search /></h1>
-        </div> 
+            <button onClick={()=> setShowDropdown(!showDropdown)}>
+                Toggle Dropdown
+            </button>
+            {showDropdown ? ( 
+            <Dropdown
+                selected={selected}
+                options={options}
+                onSelectedChange={setSelected}
+            /> ) : null
+            }
+        </div>
     );
 };
 export default App;
