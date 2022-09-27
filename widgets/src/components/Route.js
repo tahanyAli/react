35933ececa7d50de81fad7eb50050fac723174d7
tahany777 <tahanymohammed777@hotmail.com>
2022-09-27 12:10:13 +0300

    fix some proplems

--- widgets/src/components/Route.js
+++ widgets/src/components/Route.js
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+
 const Route = ({ path, children }) => {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+    useEffect(() => {
+        const onLocationChange = () => {
+            //rerender route component
+            setCurrentPath(window.location.pathname)
+        }
+        window.addEventListener('popstate', onLocationChange);
+
+        return () => {
+            window.removeEventListener('popstate', onLocationChange)
+        };
+    }, []);
     return window.location.pathname === path ? children : null;
 };
 
