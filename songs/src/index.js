9af35d91b0386c221aa61c0c977f635dcaddf895
tahany777 <tahanymohammed777@hotmail.com>
2022-10-09 16:33:50 +0300

    add the structure for redux

--- songs/src/index.js
+++ songs/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from "./reducers";
 import App from './component/App';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(<Provider store={createStore(reducers)}><App /></Provider>);
